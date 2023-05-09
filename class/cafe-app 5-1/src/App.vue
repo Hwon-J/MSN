0e6792cef93c1e10253f35c93bb2f7966361ae71
장혜원 <mins0104@naver.com>
2023-05-09 14:21:05 +0900

    te

---
+++ class/cafe-app 5-1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>Coffee Order App</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  font-family: 'Noto Sans KR', sans-serif;
+  padding: 0;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+}
+</style>
