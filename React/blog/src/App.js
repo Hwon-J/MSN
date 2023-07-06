e00d87a6981b086d78f5b2b704610158b689a7d8
origin <mins010437@gmail.com>
2023-07-07 00:26:16 +0900

    5강

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -1,4 +1,3 @@
-// import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -6,25 +5,30 @@ function App() {
 
   let post = '강남 우동 맛집';
 
-  let [글제목1, b] = useState('남자 코트 추천')
-  let [글제목2, c] = useState('강남 우동 맛집')
-  let [글제목3, d] = useState('파이썬독학')
+  let [글제목, 제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학'])
+  let [따봉, 따봉변경] = useState(0);
+
+  function change(){
+    글제목[0] = '여자 코트 추천';
+    제목변경([...글제목]);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
+      <button onClick={change}>클릭</button>
       <div className="list">
-        <h4>{ 글제목1 }</h4>
+        <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목2 }</h4>
+        <h4>{ 글제목[1] }</h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
-        <h4>{ 글제목3 }</h4>
+        <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
     </div>
@@ -32,4 +36,3 @@ function App() {
 }
 
 export default App;
-
