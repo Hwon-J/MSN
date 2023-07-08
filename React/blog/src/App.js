1211f47a1e5eedcd8b75f83f6c6972af59579172
origin <mins010437@gmail.com>
2023-07-09 00:20:53 +0900

    sat

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -5,20 +5,19 @@ function App() {
 
   let post = '강남 우동 맛집';
 
-  let [글제목, 제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학'])
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학'])
   let [따봉, 따봉변경] = useState(0);
-
-  function change(){
-    글제목[0] = '여자 코트 추천';
-    제목변경([...글제목]);
-  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <button onClick={change}>클릭</button>
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>정렬</button>
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> { 따봉 } </h4>
         <p>2월 17일 발행</p>
@@ -31,8 +30,20 @@ function App() {
         <h4>{ 글제목[2] }</h4>
         <p>2월 17일 발행</p>
       </div>
+      <Modal></Modal>
     </div>
   );
+}
+
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
