399cda4029f8ec5abe9aef2502cac07523b39d6a
origin <mins010437@gmail.com>
2023-07-16 23:55:54 +0900

    dd

--- React/shop/src/App.js
+++ React/shop/src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import {Button, Navbar, Container, Nav} from 'react-bootstrap'
+import { useState } from 'react';
+import data from './data.js';
 
 function App(){
+
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
       
@@ -18,23 +23,21 @@ function App(){
       <div className="main-bg"></div>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
+        <Item shoes={shoes[0]} i={1} />
+        <Item shoes={shoes[1]} i={2} />
+        <Item shoes={shoes[2]} i={3} />
         </div>
       </div>
+    </div>
+  )
+}
+
+function Item(props){
+  return(
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
     </div>
   )
 }
